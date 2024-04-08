8e0b337787dd93c1d8929e4eb175154cc6bf47aa
Aly Amr <aly.amr9417@gmail.com>
2024-04-08 05:49:34 +0200

    2nd commit

---
+++ test.py
@@ -0,0 +1,9 @@
+games = {
+
+    "Overwatch" : ["Shooter", "Multiplayer"],
+    "Crusader Kings 2" : ["Strategy", "Multiplayer"],
+    "The Witcher 3" : ["RPG", "Singleplayer"],
+    "Plutocracy" : ["Tycoon", "Singleplayer"],
+    "The Crew 2" : ["Racing", "Multiplayer"]
+
+}
